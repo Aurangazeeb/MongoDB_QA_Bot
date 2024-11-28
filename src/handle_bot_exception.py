b109d1c57fcb205b40f9109b0ae5f37b6eea5196
Aurangazeeb <aurangazeebak@gmail.com>
2024-11-28 21:48:19 +0530

    Organizing - automated db creation process

---
+++ src/handle_bot_exception.py
@@ -0,0 +1,14 @@
+from src.bot_exceptions import FieldMissingError, IncorrectYearError, ViolationOfDocumentStructureError, DataNotFoundError
+
+class BotResponseExceptionHandler:
+    @staticmethod
+    def handle_exception(exception_title=None, exception_message= None):
+        if exception_title:
+            if 'missing' in exception_title.lower():
+                raise FieldMissingError(exception_message)
+            elif 'year' in exception_title.lower():
+                raise IncorrectYearError(exception_message)
+            elif any([title_str for title_str in ['range', 'specific'] if title_str in exception_title.lower()]):
+                raise ViolationOfDocumentStructureError(exception_message)
+        else:
+            raise DataNotFoundError
