c203652eddfa0686f9201377a8de14dd19424ff0
Aurangazeeb <aurangazeebak@gmail.com>
2024-11-28 20:19:18 +0530

    Testing - refined test-cases by removing one repeating case

--- tests/test_system.py
+++ tests/test_system.py
@@ -11,12 +11,6 @@ def test_valid_user_query():
     valid_mongo_query_actual = query_handler.user_query_to_mongo_query(valid_user_query)['query']
     assert valid_mongo_query_expected == valid_mongo_query_actual, "Mongo query generated is different"
 
-def test_unsupported_user_query():
-    unsupported_user_query = 'What was the birth rate of Atlantis in 2023?'
-    unsupported_user_query_response_expected = 'Unsupported Query - requested information not retrievable'
-    unsupported_user_query_response_actual = query_handler.user_query_to_mongo_query(unsupported_user_query)['query']
-    assert unsupported_user_query_response_expected == unsupported_user_query_response_actual, "Mongo query generated is different"
-
 # mongo query to query result tests
 def test_querying_on_available_data():
     user_query = 'What is the total population of the region Europe in 2023?'
